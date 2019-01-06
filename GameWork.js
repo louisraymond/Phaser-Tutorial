caea55b9607beed8e47223d79366acc679d6ed24
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2019-01-06 00:21:56 +0000

    added bombs and score

--- GameWork.js
+++ GameWork.js
@@ -1,3 +1,4 @@
+
     var config = {
         type: Phaser.AUTO,
         width: 800,
@@ -17,7 +18,11 @@
     };
 
     var player;
+    var stars;
     var platforms;
+    var cursors;
+    var score = 0;
+    var scoreText;
 
     var game = new Phaser.Game(config);
 
@@ -28,17 +33,19 @@
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-
     }
 
     function create ()
     {
-        this.add.image(400, 300, 'sky');
-        cursors = this.input.keyboard.createCursorKeys();
 
+      bombs = this.physics.add.group();
+
+      this.physics.add.collider(bombs, platforms);
+
+      this.physics.add.collider(player, bombs, hitBomb, null, this);
+        this.add.image(400, 300, 'sky');
 
         platforms = this.physics.add.staticGroup();
-
 
         platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
@@ -47,9 +54,6 @@
         platforms.create(750, 220, 'ground');
 
         player = this.physics.add.sprite(100, 450, 'dude');
-
-        this.physics.add.collider(player, platforms);
-
 
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
@@ -74,34 +78,59 @@
             repeat: -1
         });
 
-        this.anims.create({
-          key: 'left',
-          frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-          frameRate: 10,
-          repeat: -1
+        cursors = this.input.keyboard.createCursorKeys();
+
+        stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
         });
+
+        stars.children.iterate(function (child) {
+
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+        });
+
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
+
+        this.physics.add.overlap(player, stars, collectStar, null, this);
     }
 
-    function update (){
-      if (cursors.left.isDown)
-      {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
-      }
-      else if (cursors.right.isDown)
-      {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-      }
-      else
-      {
-    player.setVelocityX(0);
-    player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down)
+    function update ()
     {
-    player.setVelocityY(-330);
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+
+            player.anims.play('right', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+
+            player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.setVelocityY(-330);
+        }
     }
 
+    function collectStar (player, star)
+    {
+        star.disableBody(true, true);
+
+        score += 10;
+        scoreText.setText('Score: ' + score);
     }
